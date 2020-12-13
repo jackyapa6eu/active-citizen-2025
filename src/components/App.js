543c0bf4299c544713a182b6d94ddd2cb6e4ff72
jackyapa6eu <66677379+jackyapa6eu@users.noreply.github.com>
2020-12-13 20:38:40 +0300

    Revert "Feature/cards"

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import '../index.css';
-import {
-  Switch,
-  Route
-} from "react-router-dom";
 import firebase from 'firebase/app';
 import "firebase/auth";
 import "firebase/database";
-import UserContext from '../contexts/UserContext';
-import Petitions from './Petitions';
-import User from './User';
-import Header from './Header';
-import AddNew from './AddNew';
+import poems from '../utils/poems';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBRR8gvYPh4zoGSzmQcyDz4vtkiS66NDFU",
@@ -25,16 +17,16 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const inputRef = React.useRef();
   const [petitions, setPetitions] = React.useState([]);
-  const [user, setUser] = React.useState({});
   React.useEffect(() => {
     authUser();
     getPetitions();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [])
 
   function getPetitions() {
     const petitionsRef = firebase.database().ref('petitions/');
-    petitionsRef.on('value', (snapshot) => {
+    petitionsRef.once('value', (snapshot) => {
       if (snapshot.val() === null) {
         console.log('ничего нет');
         return
@@ -53,47 +45,81 @@ function App() {
   function authUser() {
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
-        getUserData(user.uid);
+        console.log(user);
       } else {
         console.log('no user');
         }
     });   
   }
 
-  function getUserData(uid) {
-    const userRef = firebase.database().ref('users/'+ uid);
-    userRef.once('value', (snapshot) => {
-      if (snapshot.val() === null) {
-        console.log('ничего нет');
-        return
+  function findPoem(arr, str) {
+    let inputArr = str.trim().split(' ');
+    let result = {};
+    let maxCoincidences = 0;
+    for (let i = 0; i < arr.length; i++) {
+      let coincidences = 0;
+      for (let j = 0; j < inputArr.length; j++) {
+        if (arr[i].fields.text.toLowerCase().includes(inputArr[j]) && (inputArr[j].length > 2)) {
+          coincidences++;
+        }
       }
-      const obj = snapshot.val();
-      setUser(obj);
-    })
+      if (coincidences > maxCoincidences) {
+        maxCoincidences = coincidences;
+        result = arr[i];
+      }
+    }
+    return result
+  }
+  
+  function cutPoem(poem) {
+    const strArr = poem.split('\n');
+    const length = strArr.length > 15 ? 15 : strArr.length;
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += `${strArr[i]}\n`
+    }
+    return result
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    findPoem(poems, inputRef.current.value);
+    var petitionData = {
+      realText: inputRef.current.value,
+      poem: findPoem(poems, inputRef.current.value)
+    };
+    var newPetitionKey = firebase.database().ref().child('petition').push().key;
+  
+    var updates = {};
+    updates['/petitions/' + newPetitionKey] = petitionData;
+  
+    return firebase.database().ref().update(updates);
+  }
 
   return (
     <div className="App">
-      <UserContext.Provider value={user}>
-        <div className="page">
-          <Header setUser={setUser}/>
-          <main className="main">
-            <Switch>
-              <Route exact path="/">
-                <Petitions petitions={petitions}/>
-              </Route>
-              <Route path="/user">
-                <User/>
-              </Route>
-              <Route path="/add-new">
-                <AddNew/>
-              </Route>
-            </Switch>
-          </main>
-          <footer className="footer">footer</footer>
-        </div>
-      </UserContext.Provider>
+      <div className="page">
+        <header className="header">header</header>
+        <main className="main">
+          <form className="petition-form" onSubmit={handleSubmit}>
+            <input className="petition-form__input" type="text" ref={inputRef}/>
+            <button type="submit" className="petition-form__submit-btn">Найти</button>
+          </form>
+          <div className="petitions">
+          {petitions.map((petition) => {
+            return(
+              <div className="petitions__item">
+                <h3 className="petitions__title">{petition.poem.fields.name}</h3>
+                <p>Пользователь ввел:</p>
+                <p className="petitions__text">{petition.realText}</p>
+                <p>Стих (первые 15 строчек):</p>
+                <p className="petitions__text">{petition.poem.fields.text}</p>
+              </div>)
+            })}
+          </div>
+        </main>
+        <footer className="footer">footer</footer>
+      </div>
     </div>
   );
 }
