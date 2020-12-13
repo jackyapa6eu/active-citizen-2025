d87529f3dd9922073f3a172a98c4d8f96504865a
Evgenii Denisov <jackyapa6eu@yandex.ru>
2020-12-13 20:50:41 +0300

    hot-fix

--- src/components/Petition.js
+++ src/components/Petition.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import petitionImage from '../styles/images/image1.png';
+import {
+  useHistory
+} from "react-router-dom";
 
 function Petition({petition}) {
+  const history = useHistory();
+  function openPetition() {
+    history.push("/opened-petition");
+  }
   return (
-        <div className="petitions__item">
-          <div class="petitions__container petitions__container_title">
+        <div className="petitions__item" onClick={openPetition}>
+          <div className="petitions__container petitions__container_title">
             <h3 className="petitions__title">{petition.poem.fields.name}</h3>
-            <p class="petitions__data">12 декабря 2020</p>
+            <p className="petitions__data">12 декабря 2020</p>
           </div>
-          <div class="petitions__container petitions__container_rating">
-            <div class="flex">
-              <button class="btn btn_like"></button>
-              <p class="petitions__rating">1024</p>
+          <div className="petitions__container petitions__container_rating">
+            <div className="flex">
+              <button className="btn btn_like"></button>
+              <p className="petitions__rating">1024</p>
             </div>
-            <div class="flex">
-              <button class="btn btn_dislike"></button>
-              <p class="petitions__rating">362</p>
+            <div className="flex">
+              <button className="btn btn_dislike"></button>
+              <p className="petitions__rating">362</p>
             </div>
           </div>
-          <img src={petitionImage} alt="" class="petitions__image"/>
+          <img src={petitionImage} alt="" className="petitions__image"/>
         </div>
         )
 }
