e1812ae9a118c52582327c946ff4159e83565954
Evgenii Denisov <jackyapa6eu@yandex.ru>
2020-12-26 15:10:18 +0300

    petitions list for mobile

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,41 +1,8 @@
 import React from 'react';
 import Category from './Category';
-
+import {categoriesData} from '../utils/categories.js';
 function Categories({setSelectedCategory, selectedCategory, place}) {
-  const categoriesData = [
-  { 
-    value: 'all',
-    text: 'Все',
-  },
-  { 
-    value: 'culture',
-    text: 'Культура',
-  },
-  { 
-    value: 'homeland',
-    text: 'Родина',
-  },
-  { 
-    value: 'history',
-    text: 'История',
-  },
-  { 
-    value: 'science',
-    text: 'Наука',
-  },
-  { 
-    value: 'nature',
-    text: 'Природа',
-  },
-  { 
-    value: 'family',
-    text: 'Семья',
-  },
-  { 
-    value: 'relationship',
-    text: 'Отношения',
-  }
-  ]
+
   return (
     <ul className={`categories categories_place_${place}`}>
       {categoriesData.map((categoryItem, index) => {
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Categories from './Categories';
 import Petitions from './Petitions';
+import { categoriesMobileData } from '../utils/categories';
+import classNames from 'classnames'
 
 
 function Feed({petitions, showDate}) {
   const [selectedCategory, setSelectedCategory] = React.useState('all');
+  const [isMobileCategoriesOpened, setisMobileCategoriesOpened] = React.useState(false);
+  const catigoriesMobileContainerSelectors = classNames (
+    'catigories__mobile-container',
+    {
+      'catigories__mobile-container_opened': isMobileCategoriesOpened
+    }
+  )
+
+  function toggleCategories() {
+    setisMobileCategoriesOpened(!isMobileCategoriesOpened);
+  }
+  
   return (
-    <>
+    <>  
+      <div className={catigoriesMobileContainerSelectors}>
+        <button className="categories__popup-btn" onClick={toggleCategories}>Темы</button>
+        <span className="categories__selected">{categoriesMobileData[selectedCategory]}</span>
+        <Categories setSelectedCategory={setSelectedCategory} selectedCategory={selectedCategory} place={'feed-mobile'}/>
+      </div>
       <Categories setSelectedCategory={setSelectedCategory} selectedCategory={selectedCategory} place={'feed'}/>
       <Petitions petitions={petitions} showDate={showDate} selectedCategory={selectedCategory}/>
     </>
